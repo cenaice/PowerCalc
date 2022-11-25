5028f1406d9f337621c67842ab5c245645fd685c
victor <victorphiathep@gmail.com>
2022-11-25 16:46:54 -0500

    Adding starting files

--- README.md
+++
@@ -1,3 +0,0 @@
-# PowerCalc
-
-Program to calculate your WILKS, 1RM, and other lifting needs. Compare yourselves to how other athletes performed in a powerlifting meet.
---
+++ powercalc/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+  <h1 className="text-3xl font-bold underline">
+    Hello nigga
+  </h1>
+  );
+}
+
+export default App;
