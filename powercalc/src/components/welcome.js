254b39fb32e6b1075cba723b1dd7ff4ffaa10af8
Victor <victorphiathep@gmail.com>
2022-12-06 12:36:28 -0500

    Changed landing page

--- powercalc/src/components/welcome.js
+++ powercalc/src/components/welcome.js
@@ -3,7 +3,7 @@ import React from "react";
 function Welcome() {
   return (
     <div class="flex p-7 m-2 bg-red">
-      <h1>Welcome NIGGGGA</h1>
+      <h1>Landing Page Test</h1>
     </div>
   );
 };
